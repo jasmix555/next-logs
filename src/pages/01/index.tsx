84d25c4739db5a8167388421fc83f6f0d3d6095e
jasmix555 <94930720+jasmix555@users.noreply.github.com>
2024-01-01 17:24:45 +0900

    Add to-do list functionality and update page transitions

--- src/pages/01/index.tsx
+++ src/pages/01/index.tsx
@@ -1,3 +1,84 @@
+import Layout from "../component/Layout";
+import { useState } from "react";
+import style from "@/styles/styles.module.scss";
+
 export default function Page01() {
-  return <h1>Page 01</h1>;
+  type todoListType = {
+    text: string;
+    list: string[];
+  };
+
+  const [toDo, setoDo] = useState<todoListType>({
+    text: "",
+    list: [],
+  });
+
+  return (
+    <Layout>
+      <h1>Roulette</h1>
+      <div className={"wrapper"}>
+        <div className="contentWrapper">
+          <div className="insert">
+            <input
+              type="text"
+              placeholder="Insert text"
+              className={style.input}
+              name="item"
+              id="item"
+              value={toDo.text}
+              onKeyDown={(e) => {
+                if (e.key === "Enter" && toDo.text.trim() !== "") {
+                  setoDo({
+                    ...toDo,
+                    list: [...toDo.list, toDo.text],
+                    text: "",
+                  });
+                }
+              }}
+              onChange={(e) => {
+                setoDo({
+                  ...toDo,
+                  text: e.target.value,
+                });
+              }}
+            />
+            <button
+              disabled={toDo.text === ""}
+              className={style.addBtn}
+              onClick={() => {
+                setoDo({
+                  text: "",
+                  list: [...toDo.list, toDo.text],
+                });
+              }}
+            >
+              Add
+            </button>
+          </div>
+          <div className="list">
+            <ul className={style.ul}>
+              {toDo.list.map((item, idx) => {
+                return (
+                  <li key={idx} className={style.li}>
+                    {item}
+                    <button
+                      className={style.delBtn}
+                      onClick={() => {
+                        setoDo({
+                          ...toDo,
+                          list: toDo.list.filter((_, i) => i !== idx),
+                        });
+                      }}
+                    >
+                      X
+                    </button>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,18 +12,26 @@ export default function App({ Component, pageProps }: AppProps) {
         <Component {...pageProps} />
         <motion.div
           className="slide-in"
-          initial={{ scaleY: 0 }}
-          animate={{ scaleY: 0 }}
-          exit={{ scaleY: 1 }}
+          initial={{ scaleX: 0 }}
+          animate={{ scaleX: 0 }}
+          exit={{ scaleX: 1 }}
           transition={{ duration: 0.5, ease: [0.22, 1, 0.36, 1] }}
         ></motion.div>
 
         <motion.div
+          className="slide-in-one"
+          initial={{ scaleX: 0 }}
+          animate={{ scaleX: 0 }}
+          exit={{ scaleX: 1 }}
+          transition={{ duration: 0.5, ease: [0.22, 1, 0.36, 1], delay: 0.3 }}
+        ></motion.div>
+
+        <motion.div
           className="slide-out"
-          initial={{ scaleY: 1 }}
-          animate={{ scaleY: 0 }}
-          exit={{ scaleY: 0 }}
-          transition={{ duration: 0.5, ease: [0.22, 1, 0.36, 1] }}
+          initial={{ scaleX: 1 }}
+          animate={{ scaleX: 0 }}
+          exit={{ scaleX: 0 }}
+          transition={{ duration: 0.5, ease: [0.22, 1, 0.36, 1], delay: 0.2 }}
         ></motion.div>
       </motion.div>
     </AnimatePresence>
